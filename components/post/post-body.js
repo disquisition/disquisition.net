32e26e68c99a645e22df33bdf58e3d2e79313f41
Spenser Isdahl <scisdahl@gmail.com>
2017-08-29 15:17:31 -0500

    Refactor PostBody to move componentAlias to Post

--- components/post/post-body.js
+++ components/post/post-body.js
@@ -1,30 +1,14 @@
-import A from '../anchor';
-import Code from '../code';
-import Hr from '../horizontal-rule';
-import P from '../paragraph';
 import PropTypes from 'prop-types';
 import React from 'react';
-import SectionHeader from './section-header';
-import Sup from '../superscript';
 import toH from 'hast-to-hyperscript';
 
-const componentAliases = {
-  a: A,
-  code: Code,
-  h1: () => null,
-  h2: SectionHeader,
-  hr: Hr,
-  p: P,
-  sup: Sup
-};
-
-const createElement = (name, attrs, children) => {
-  const component = componentAliases[name];
+const createElementFactory = (aliases = {}) => (name, attrs, children) => {
+  const component = aliases[name];
 
   return React.createElement(component || name, attrs, children);
 };
 
-const PostBody = ({ hast }) => {
+const PostBody = ({ aliases, hast }) => {
   // toH requires an element at the HAST root
   if (hast.type === 'root') {
     hast = Object.assign({}, hast, {
@@ -33,10 +17,11 @@ const PostBody = ({ hast }) => {
     });
   }
 
-  return toH(createElement, hast);
+  return toH(createElementFactory(aliases), hast);
 };
 
 PostBody.propTypes = {
+  aliases: PropTypes.object,
   hast: PropTypes.object.isRequired
 };
 
--- pages/post.js
+++ pages/post.js
@@ -1,4 +1,5 @@
 import A from '../components/anchor';
+import Code from '../components/code';
 import Hr from '../components/horizontal-rule';
 import Page from '../layouts/main';
 import PostBody from '../components/post/post-body';
@@ -6,11 +7,24 @@ import PostHead from '../components/post/post-head';
 import PostHeader from '../components/post/post-header';
 import PostHeroImage from '../components/post/post-hero-image';
 import PostFooter from '../components/post/post-footer';
+import SectionHeader from '../components/post/section-header';
+import P from '../components/paragraph';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Sup from '../components/superscript';
 import fetch from 'isomorphic-unfetch';
 import getOrigin from '../lib/get-origin';
 import { Article, Div } from 'glamorous';
+
+const componentAliases = {
+  a: A,
+  code: Code,
+  h1: () => null, // This gets rendered in the PostHeader instead
+  h2: SectionHeader,
+  hr: Hr,
+  p: P,
+  sup: Sup
+};
 
 export default class Post extends React.Component {
   static async getInitialProps({ query: { slug }, req }) {
@@ -57,7 +71,7 @@ export default class Post extends React.Component {
           <Article lineHeight={1.6}>
             <PostHeader date={post.date}>{post.title}</PostHeader>
 
-            <PostBody hast={post.content} />
+            <PostBody hast={post.content} aliases={componentAliases} />
 
             <Hr />
 
