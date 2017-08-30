8ec102b4ebbf1bbc95b06fd383f645bee824ce47
Spenser Isdahl <scisdahl@gmail.com>
2017-08-29 22:48:56 -0500

    Add ids to post h1s

--- components/post/post-header.js
+++ components/post/post-header.js
@@ -1,11 +1,27 @@
 import PropTypes from 'prop-types';
-import glamorous, { H1 } from 'glamorous';
+import glamorous from 'glamorous';
+
+function toId(children) {
+  return typeof children === 'string'
+    ? children
+      .toLowerCase()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+    : null;
+}
+
+const PostHeading = glamorous
+  .h1({
+    marginTop: '1rem',
+    marginBottom: 0
+  })
+  .withProps(({ children }) => ({
+    id: toId(children)
+  }));
 
 const PostHeader = ({ children, className, date }) => (
   <header className={className}>
-    <H1 marginTop="1rem" marginBottom={0}>
-      {children}
-    </H1>
+    <PostHeading>{children}</PostHeading>
 
     <span>{date}</span>
   </header>
